c8aa6ef6ada0124740e4fafbccf1ae408770a119
Yunyi Shen <34624472+YunyiShen@users.noreply.github.com>
2019-02-18 18:00:17 -0600

    Update Moller_repeat_try.r

--- R/Moller_repeat_try.r
+++ R/Moller_repeat_try.r
@@ -2,25 +2,25 @@ require('gtools')
 set.seed(42)
 source('Moller_repeat.R')
 #source("misc.R")
-nlat = 20
+nlat = 30
 siteposi = 1.00 * permutations(n=nlat,r=2,v=(1:nlat),repeats.allowed = T)
 
 distM = as.matrix((dist(siteposi)))
-distM=distM
+distM=distM-1
 diag(distM)=0
-dist_thr = 5
+dist_thr = 50
 #distM = 1*(distM==1)
 
 ones = rep(1,times = nlat*nlat)
 X = cbind(ones)
-theta = matrix(c(-.0,0.15,.7))
+theta = matrix(c(-.0,0.1,2.5))
 
 
 set.seed(42)
 Zsample = rIsing(X,distM,theta,method = "CFTP",nIter=100,n=1,dist_thr)
 raster::plot(raster::raster(matrix(Zsample,nlat,nlat)))
 
-var_prop = c(2.5e-5,2.5e-5,1e-4)
+var_prop = c(1e-3,1e-4,1e-3)
 
 kk=Moller.sampler_repeat(X=X,distM=distM,
                                       #detmat = detmat, 
