809766d4d38630f4c79679aa1805fa13abfc02db
YunyiShen <yshen99@outlook.com>
2019-03-18 23:10:14 -0500

    continue island

--- R/Island Occu/misc_island.R
+++ R/Island Occu/misc_island.R
@@ -149,24 +149,41 @@ rIsingOccu_multi = function(theta,envX,distM,link_map,dist_mainland,link_mainlan
 }
   # passed
 
-Pdet_multi = function(detmat, envX,detX, beta_det, nspp){ # likelihood given Z and detections If have repeat, use this multiple times.
-
-
+Pdet_multi = function(nperiod, envX,detX, beta_det, nspp){ # likelihood given Z and detections If have repeat, use this multiple times.
 	# this is still 2 spp case, need to change to multi case
-	nperiod = ncol(detmat) # detmat is the data of 0 and 1 for detections
+	#nperiod = ncol(detmat) # detmat is the data of 0 and 1 for detections
 	# length(beta_det) = 2 * ncol(detX[[1]]) + 2 * ncol(X)  # beta for detections
-	detDesign = lapply(detX,function(x,y){cbind(y,x)},y = envX) # This is the full design matrix list of detection probability p at time
+	detDesign = lapply(detX,function(x,y){ cbind(y,x)},y = envX) # This is the full design matrix list of detection probability p at time
 	npardet = ncol(detDesign[[1]])
-	P_det = matrix(0,nrow = 1,ncol = nperiod)
-	P_det = P_det[,-1]
-	for(i in 1:nspp){
-		Xbeta_temp = lapply(detDesign,function(w,beta1){w%*%beta1},beta1 = matrix( beta_det[1:ncol(detX) + (i-1) * ncol(detX)]))
-		P_det_temp = lapply(Xbeta_temp,function(W){exp(W) / (1 + exp(W))}) # just a logistic regression for detection
-		P_det_temp = (matrix(unlist(P_det_temp),nrow = nrow(X),ncol = nperiod)) # detection probability, row is site i col is period j
-		P_det = rbind(P_det,P_det_temp)
-	}
+	#Pdet = list()
+	#nrep = length(detX)
+	#for(j in 1:nrep){
+	  P_det = matrix(0,nrow = 1,ncol = nperiod)
+	  P_det = P_det[-1,]
+	  for(i in 1:nspp){
+		  Xbeta_temp = lapply(detDesign,function(w,beta1){w%*%beta1},beta1 = matrix( beta_det[1:npardet + (i-1) * npardet]))
+		  P_det_temp = lapply(Xbeta_temp,function(W){exp(W) / (1 + exp(W))}) # just a logistic regression for detection
+		  P_det_temp = (matrix(unlist(P_det_temp),nrow = nrow(envX),ncol = nperiod)) # detection probability, row is site i col is period j
+		  P_det = rbind(P_det,P_det_temp)
+	  }
+	  #Pdet[[j]] = P_det
+	#}
 	return(P_det)
 }
+  # passed, will return a matrix, with nrow = nspp*nsite, ncol = nperiod,
+  #  1:nsite for species 1 and 1:nsite+(i-1)*nsite rows for species i.
+
+Sample_detection = function(nrep,nperiod,envX,detX,beta_det,nspp){
+  detmat = list()
+  nsite = nrow(envX)
+  for(i in 1:nrep){
+    r = matrix( runif(nperiod * nspp * nsite) , nspp * nsite,nperiod )
+    Pdet = Pdet_multi(nperiod, envX,detX[[i]], beta_det, nspp)
+    detmat[[i]] = 1.0 * (Pdet<=r)
+  }
+  return(detmat)
+}
+  # passed
 
 IsingOccu_multi.logL.innorm = function(theta, envX, distM,link_map,dist_mainland,link_mainland,int_range_intra="nn",int_range_inter="exp", Z ,detmat, detX){ # the in-normalized log likelihood of IsingOccu Model beta is matrix here detX should be a list of list detmat should be a list, they should have the same length
 	nspp = nrow(theta$spp_mat)
@@ -176,13 +193,14 @@ IsingOccu_multi.logL.innorm = function(theta, envX, distM,link_map,dist_mainland
 	#beta_det = matrix(detbeta,nrow = length(detbeta),ncol = 1)#
 	logLdata=0
 	for(i in 1:nrep){
-		P_det = Pdet_multi(detmat[[i]], detX[[i]], beta_det,nspp) # detX should be a list of list 
+		P_det = Pdet_multi(ncol(detmat[[i]]), envX,detX[[i]], beta_det,nspp) # detX should be a list of list 
 		LP_Z1 = as.matrix(rowSums(detmat[[i]] * log(P_det) + (1-detmat[[i]]) * log(1-P_det)))
 		LP_Z0 = as.matrix(log(1*(rowSums(detmat[[i]])==0) + 1e-13 * (1-(rowSums(detmat[[i]])==0)))) # I(data = 0), do not want err for those have detections
 		logLdata = logLdata +  sum(as.numeric((Z[,i]+1)/2) * LP_Z1 + as.numeric(1-((Z[,i]+1)/2)) * LP_Z0)
 	}
 	return(negPot+logLdata)
 }
+  # passed
 
 Moller.ratio = function(theta_curr ,theta_prop
 						,Z_curr ,Z_prop
@@ -196,8 +214,9 @@ Moller.ratio = function(theta_curr ,theta_prop
 						,int_range_intra="nn",int_range_inter="exp"){
 	log_H_theta_tuta_Z_temp_prop = Hamiltonian(theta_tuta,envX,distM,link_map,dist_mainland,link_mainland,int_range_intra,int_range_inter,Z_temp_prop)
 	# then auxiliented variable x_prop is same to detmat, together with Z_temp_prop from underlaying Isingmodel. It was proposed using likelihood function with parameter theta_prop and in the main sampler, which is important in canceling out the normalizing constant.
-	log_pi_theta_prop =log(dnorm(theta_prop,0,sd=sqrt(vars_prior)))
-	log_pi_theta_prop = sum(log_pi_theta_prop)
+	log_pi_theta_prop = lapply(theta_prop,function(theta_temp,vars_prior){ sum(log(dnorm(theta_temp,0,sd=sqrt(vars_prior))))},vars_prior)
+	log_pi_theta_prop = sum(unlist(log_pi_theta_prop))
+	#log_pi_theta_prop = sum(log_pi_theta_prop)
 	#prior of proposed theta
 	log_q_theta_Z_prop_detmat = IsingOccu_multi.logL.innorm(theta_prop, envX, distM, link_map,dist_mainland,link_mainland,int_range_intra,int_range_inter,Z_prop ,detmat = detmat, detX)
 	# theta_prop should be sample from independent Gaussian distribution with mean theta_curr, Z_prop should be directly sample from a uniform configuration (of course where exist detection should be 1 with probability 1, actually sample all 0s, then we can cancel out the proposal probability from the MH ratio)
@@ -206,9 +225,9 @@ Moller.ratio = function(theta_curr ,theta_prop
 	#### end of the upper part, start the lower
 
 	log_H_theta_tuta_Z_temp_curr = Hamiltonian(theta_tuta,envX,distM,link_map,dist_mainland,link_mainland,int_range_intra,int_range_inter,Z_temp_curr)
-	log_pi_theta_curr =log(dnorm(theta_curr,0,sd=sqrt(vars_prior)))
-	log_pi_theta_curr = sum(log_pi_theta_curr)
-	log_q_theta_Z_curr_detmat = IsingOccu_multi.logL.innorm(theta_curr, envX, distM, dist_mainland,link_mainland,link_map,int_range_intra,int_range_inter,Z_curr ,detmat = detmat, detX)
+	log_pi_theta_curr = lapply(theta_curr,function(theta_temp,vars_prior){ sum(log(dnorm(theta_temp,0,sd=sqrt(vars_prior))))},vars_prior)
+	log_pi_theta_curr = sum(unlist(log_pi_theta_curr))
+	log_q_theta_Z_curr_detmat = IsingOccu_multi.logL.innorm(theta_curr, envX, distM, link_map,dist_mainland,link_mainland,int_range_intra,int_range_inter,Z_curr ,detmat = detmat, detX)
 	log_H_theta_Z_temp_prop = Hamiltonian(theta_prop,envX,distM,link_map,dist_mainland,link_mainland,int_range_intra,int_range_inter,Z_temp_prop)
 
 	log_MH_ratio = (log_H_theta_tuta_Z_temp_prop + log_pi_theta_prop + log_q_theta_Z_prop_detmat + log_H_theta_Z_temp_curr)-
--- R/Island Occu/test.R
+++ R/Island Occu/test.R
@@ -1,17 +1,17 @@
 source("misc_island.R")
 
 ###### graph data ######
-island = read.csv("C:/Users/yshen99/Documents/GitHub/RFIBM_MCMC/Island/CT_posi_only_island.csv")
+island = read.csv("E:/UW Lab jobs/2. ISING Occupancy model/2. RFIBMs MCMC/RFIBM/island/CT_posi_only_island.csv")
 
-link_inner = as.matrix( read.csv("C:/Users/yshen99/Documents/GitHub/RFIBM_MCMC/Island/link_inner.csv",row.names = 1))
+link_inner = as.matrix( read.csv("E:/UW Lab jobs/2. ISING Occupancy model/2. RFIBMs MCMC/RFIBM/island/link_inner.csv",row.names = 1))
 #link_outer = as.matrix( read.csv("link_outer.csv",row.names = 1))
-link_outer = as.matrix( read.csv("C:/Users/yshen99/Documents/GitHub/RFIBM_MCMC/Island/link_outer_full.csv",row.names = 1))
-link_mainland = as.matrix( read.csv("C:/Users/yshen99/Documents/GitHub/RFIBM_MCMC/Island/link_mainland.csv"))
+link_outer = as.matrix( read.csv("E:/UW Lab jobs/2. ISING Occupancy model/2. RFIBMs MCMC/RFIBM/island/link_outer_full.csv",row.names = 1))
+link_mainland = as.matrix( read.csv("E:/UW Lab jobs/2. ISING Occupancy model/2. RFIBMs MCMC/RFIBM/island/link_mainland.csv"))
 #link_outer = 0 * link_outer # this makes it a mainland-island system
 #link_mainland = matrix(0,155,1)
 
-distM_full = as.matrix( read.csv("C:/Users/yshen99/Documents/GitHub/RFIBM_MCMC/Island/distM_full.csv",row.names = 1))
-distM_mainland = as.matrix( read.csv("C:/Users/yshen99/Documents/GitHub/RFIBM_MCMC/Island/dist_to_mainland.csv",row.names = 1))
+distM_full = as.matrix( read.csv("E:/UW Lab jobs/2. ISING Occupancy model/2. RFIBMs MCMC/RFIBM/island/distM_full.csv",row.names = 1))
+distM_mainland = as.matrix( read.csv("E:/UW Lab jobs/2. ISING Occupancy model/2. RFIBMs MCMC/RFIBM/island/dist_to_mainland.csv",row.names = 1))
 
 intcd = min(min((distM_mainland*link_mainland)[(distM_mainland*link_mainland)>0]),
             min((link_outer*distM_full)[(link_outer*distM_full)>0]))
@@ -27,6 +27,7 @@ spp_mat = matrix(c(0,1,1,0),2,2)
 envX = matrix(1,155,1)
 
 theta = list(beta_occu = c(0,0),
+             beta_det = c(0,1,-1,0,1,-1),
              eta_intra = c(.15,.15),
              eta_inter = c(.3,.3),
              d_inter = c(.2,.2),
@@ -36,13 +37,14 @@ link_map =
   list(inter = link_outer,
        intra = link_inner)
 
+nrep = 4
 set.seed(42)
 Z_sample = rIsingOccu_multi(theta,
                             envX,
                             distM_full,link_map ,
                             distM_mainland,link_mainland,
                             int_range_intra="nn",int_range_inter="exp",
-                            n=4,method = "CFTP",nIter = 100)
+                            n=nrep,method = "CFTP",nIter = 100)
 
 require(ggplot2)
 
@@ -55,3 +57,42 @@ ggplot(data = tempdata,aes(x=X,y=Y,color = Z_1))+
 
 Hamiltonian(theta,envX,distM_full,link_map,distM_mainland,link_mainland,int_range_intra="nn",int_range_inter="exp",Z_sample)
 
+
+detX = list()
+nperiod = 10
+for(j in 1:nrep){
+  detX[[j]] = list()
+  for (i in 1:nperiod){
+    temp = matrix(runif(155 * 2),nrow = 155,ncol = 2)
+    detX[[j]][[i]] = temp
+  }
+}
+
+Pdet = Pdet_multi(nperiod, envX,detX[[1]], theta$beta_det, nspp=nrow(spp_mat))
+
+detmat = Sample_detection(nrep,nperiod,envX,detX,theta$beta_det,nspp = nrow(spp_mat))
+
+H = IsingOccu_multi.logL.innorm(theta, envX, distM_full,link_map,distM_mainland,link_mainland,int_range_intra="nn",int_range_inter="exp", Z_sample ,detmat, detX)
+
+theta_prop = list(beta_occu = c(0.01,0.01),
+                  beta_det = c(0,1,-1,0,1,-1),
+                  eta_intra = c(.15,.151),
+                  eta_inter = c(.31,.3),
+                  d_inter = c(.21,.2),
+                  spp_mat = -0.13 * spp_mat)
+Z_temp_prop = rIsingOccu_multi(theta_prop,
+                               envX,
+                               distM_full,link_map ,
+                               distM_mainland,link_mainland,
+                               int_range_intra="nn",int_range_inter="exp",
+                               n=nrep,method = "CFTP",nIter = 100)
+M_ratio = Moller.ratio(theta_curr = theta ,theta_prop
+                        ,Z_curr = Z_sample ,Z_prop = Z_sample
+                        ,Z_temp_curr = Z_sample, Z_temp_prop
+                        ,detmat
+                        ,vars_prior = 10000
+                        ,theta_tuta = theta
+                        ,envX, detX
+                        ,distM_full,link_map
+                        ,distM_mainland,link_mainland
+                        ,int_range_intra="nn",int_range_inter="exp")
