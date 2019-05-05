4d9a08fe355fa05a0ff68de8b2bacf533c50e6fa
YunyiShen <yshen99@outlook.com>
2019-05-04 22:43:50 -0500

    add test

--- R/FOX_Ising_det.R
+++ R/FOX_Ising_det.R
@@ -25,17 +25,20 @@ normd = max(max(distM_mainland*link_mainland),max(link_outer*distM_full))-intcd
 distM_full = (distM_full-intcd)/normd # normalizing the distance
 distM_mainland = (distM_mainland-intcd)/normd
 
+detmat = list(as.matrix(read.csv(paste0(link,"det_his_coyote_fox_bobcat_20day_2015.csv"),header = F)))
+
 ###### simulation ######
 
-spp_mat = matrix(c(0,1,1,0),2,2)
+spp_mat = matrix(1,3,3)
+diag(spp_mat)=0
 envX = matrix(1,155,1)
 
-theta = list(beta_occu = c(0,0),
-             beta_det = c(0,0),
-             eta_intra = c(.2,.2),
-             eta_inter = c(.2,.2),
+theta = list(beta_occu = c(0,0,0),
+             beta_det = c(0,0,0),
+             eta_intra = c(.2,.2,.2),
+             eta_inter = c(.2,.2,.2),
              #d_inter = c(.2,.2),
-             spp_mat = 0.4 * spp_mat,
+             spp_mat = 0.3 * spp_mat,
              spp_mat_det = -0.3 * spp_mat)
 
 link_map = 
@@ -43,25 +46,6 @@ link_map =
        intra = link_inner)
 
 nrep = 1
-set.seed(42)
-Z_sample = rIsingOccu_multi(theta,
-                            envX,
-                            distM_full,link_map ,
-                            distM_mainland,link_mainland * exp(-distM_mainland) ,
-                            int_range_intra="nn",int_range_inter="nn",
-                            n=nrep,method = "CFTP",nIter = 100)
-
-require(ggplot2)
-
-tempdata = data.frame(island[,6:7],
-                      Z_1 = Z_sample[1:155,1],
-                      Z_2 = Z_sample[156:310,1])
-
-ggplot(data = tempdata,aes(x=X,y=Y,color = Z_1))+
-  geom_point()
-
-Hamiltonian(theta,envX,distM_full,link_map,distM_mainland,link_mainland*exp(-distM_mainland),int_range_intra="nn",int_range_inter="nn",(Z_sample))
-
   #    then in the second level list, it is a matrix with nrow = site ncol = ncov, 
 
 #Pdet = Pdet_multi(nperiod, envX,detX[[1]], theta$beta_det, nspp=nrow(spp_mat))
@@ -72,27 +56,10 @@ Hamiltonian(theta,envX,distM_full,link_map,distM_mainland,link_mainland*exp(-dis
 #sppmat_det = -0.1 * spp_mat
 #Pdet_Ising(nperiod,envX,detX[[1]],beta_det = theta$beta_det,sppmat_det,Z = Z_sample,detmat[[1]])
 
-nperiod = 10
-nsite = 155
-nspp = 2
-detmat = list(matrix(-1,nsite*nspp,nperiod))
 
-no_obs=11
-set.seed(42)
-detmat = Sample_Ising_detection_rep(1,nperiod,envX,detX=NULL,beta_det = theta$beta_det,theta$spp_mat_det,Z = Z_sample,detmat,nIter=100,n=1, method = "CFTP")
-Pdet_Ising_rep(1,nperiod,envX,detX=NULL,beta_det = theta$beta_det,theta$spp_mat_det,Z = Z_sample,detmat)
+no_obs=150:155
 
-detmat[[1]][c(no_obs,no_obs+155),]=-1
-
-tempdata = data.frame(island[,6:7],
-                      Z_1 = detmat[[1]][1:155,1],
-                      Z_2 = detmat[[1]][156:310,1])
-
-ggplot(data = tempdata,aes(x=X,y=Y,color = Z_1))+
-  geom_point()
-
-
-nspp = 2
+nspp = 3
 
 vars_prop = list( beta_occu = rep(1e-3,nspp * ncol(envX))
                   ,beta_det = rep(2.5e-3,nspp * ( ncol(envX)) ) # no extra det thing
@@ -115,13 +82,13 @@ kk = IsingOccu.fit.Murray.sampler_Ising_det(X = envX, detmat =  detmat,no_obs = 
                                   , mcmc.iter = 5000, burn.in = 500
                                   , vars_prop = vars_prop
                                   , vars_prior = 200000
-                                  , Zprop_rate = 0
+                                  , Zprop_rate = 0.5
                                   , Zprop_rate_missing_obs = 0.5
                                   , distM=distM_full,link_map=link_map
                                   , dist_mainland =  distM_mainland , link_mainland =  link_mainland * exp(-distM_mainland)
                                   , int_range_intra="nn",int_range_inter="nn"
-                                  , Z = Z_sample # just used in formating, if assuming perfect detection, simple giving Z and set Zprop_rate=0
-                                  #, Z = Z_absolute
+                                  #, Z = Z_sample # just used in formating, if assuming perfect detection, simple giving Z and set Zprop_rate=0
+                                  , Z = Z_absolute
                                   , seed = 42
                                   , ini = theta,thin.by = 1,report.by = 100)
 
