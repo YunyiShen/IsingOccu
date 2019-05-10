c46ef2108055976f145f9b216fd2acead77e0bc9
Yunyi Shen <34624472+YunyiShen@users.noreply.github.com>
2019-05-10 13:34:59 -0500

    Update FOX_Ising_det.R

--- R/FOX_Ising_det.R
+++ R/FOX_Ising_det.R
@@ -66,8 +66,8 @@ nspp = 3
 
 vars_prop = list( beta_occu = rep(5e-4,nspp * ncol(envX))
                   ,beta_det = rep(2.5e-3,nspp * ( ncol(envX)) ) # no extra det thing
-                  ,eta_intra = rep(1e-4,nspp)
-                  ,eta_inter = rep(1e-4,nspp)
+                  ,eta_intra = rep(5e-4,nspp)
+                  ,eta_inter = rep(5e-4,nspp)
                   #,d_intra=rep(2.5e-5,nspp)
                   #,d_inter = rep(1e-4,nspp)
                   ,spp_mat = 5e-4
