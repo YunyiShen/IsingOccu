ff3b756d5db20ac3f29a5742e19edbbba289a349
Yunyi Shen <34624472+YunyiShen@users.noreply.github.com>
2019-06-29 22:08:31 -0500

    some res

--- R/FISHER_Ising_det.R
+++ R/FISHER_Ising_det.R
@@ -64,10 +64,10 @@ nrep = 1
 
 nspp = 2
 
-vars_prop = list( beta_occu = rep(1e-3,nspp * ncol(envX))
+vars_prop = list( beta_occu = rep(2.5e-3,nspp * ncol(envX))
                   ,beta_det = rep(2.5e-3,nspp * ( ncol(envX)) ) # no extra det thing
                   ,eta_intra = rep(4e-4,nspp)
-                  ,eta_inter = rep(4e-4,nspp)
+                  ,eta_inter = rep(2.5e-3,nspp)
                   #,d_intra=rep(2.5e-5,nspp)
                   #,d_inter = rep(1e-4,nspp)
                   ,spp_mat = 2.5e-3
@@ -82,7 +82,7 @@ Z_absolute = (sapply(detmat_0,function(detmat_i){rowSums((detmat_i+1)/2)>0})) * 
 
 kk = IsingOccu.fit.Murray.sampler_Ising_det(X = envX, detmat =  detmat
                                   , detX =  NULL
-                                  , mcmc.iter = 50000, burn.in = 1500
+                                  , mcmc.iter = 50000, burn.in = 5000
                                   , vars_prop = vars_prop
                                   , vars_prior = 200000
                                   , Zprop_rate = 0.05
@@ -96,7 +96,7 @@ kk = IsingOccu.fit.Murray.sampler_Ising_det(X = envX, detmat =  detmat
                                   , ini = theta,thin.by = 10,report.by = 500,nIter = 30)
 
 
-
+save.image("FM_Mainland_island.RData")
 
 
 
