7fd6a33b97a43e99d5ec74ca2035add2430655ba
Yunyi Shen <34624472+YunyiShen@users.noreply.github.com>
2019-09-08 20:01:58 -0500

    minor change

--- R/est_logLik.R
+++ R/est_logLik.R
@@ -158,7 +158,7 @@ deltaDIC = function(theta_a_mcmc,envX_a,distM,link_map_a,dist_mainland,link_main
 	pD_Gelman04_a = 2 * var(logLik_a)
 	pD_Gelman04_t = 2 * var(logLik_theta)
     
-	deltaDIC = - 2 * mean(logLik_a) + 2 * (pD_Gelman04_a) +  mean(logLik_theta) - 2 * (pD_Gelman04_t)
+	deltaDIC = - 2 * mean(logLik_a) +  (pD_Gelman04_a) +  2 * mean(logLik_theta) -  (pD_Gelman04_t)
 	
 	robust_cri = list(robust_a = Reduce(mean,robust_cri_a),robust_theta = Reduce(mean,robust_cri_theta))
 	
